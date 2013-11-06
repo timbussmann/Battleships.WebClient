141bfab7d571d3adca7b52e56a9c9fabe93ff1f6
timbussmann <notthemail@youarelooking.for>
2013-11-06 21:19:47 +0100

    removed userService and userController

--- app/scripts/controllers/userController.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-angular.module('battleships').controller('userController', function($scope, userService){
-    $scope.username = userService.username; //todo there is no automatic two way binding for username. use watch or introduce dto object
-});
--- app/scripts/userService.js
+++
@@ -1,3 +0,0 @@
-angular.module('battleships').service('userService', function(){
-    this.username = 'player' + Math.floor(Math.random() * 10001);
-});
